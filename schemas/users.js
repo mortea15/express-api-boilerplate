21e7075a1033c9bdddb5daaeb64a237c863090b9
mortea15 <mortea15@student.uia.no>
2019-01-12 17:41:39 +0100

    renamed mongoose schemas dir

---
+++ schemas/users.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const environment = process.env.NODE_ENV
+const stage = require('../config')[environment]
+const logger = require('../helpers/logger')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  email: {
+    type: 'String',
+    required: true,
+    unique: true
+  },
+  password: {
+    type: 'String',
+    required: true,
+    trim: true
+  },
+  active: {
+    type: 'Boolean',
+    default: false
+  },
+  lastLogin: {
+    type: Date,
+    default: Date.now
+  },
+  admin: {
+    type: 'Boolean',
+    default: false
+  }
+})
+
+userSchema.pre('save', function (next) {
+  const user = this
+  if (!user.isModified || !user.isNew) {
+    next()
+  } else {
+    bcrypt.hash(user.password, stage.saltingRounds, function (err, hash) {
+      if (err) {
+        logger.log('error', `Error hashing password for user ${user.email}`)
+        next(err)
+      } else {
+        user.password = hash
+        next()
+      }
+    })
+  }
+})
+
+module.exports = mongoose.model('User', userSchema) // instance of schema
