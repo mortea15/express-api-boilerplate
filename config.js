aa10d208606adf607788af91f058c9fc150a6c3a
mortea15 <mortea15@student.uia.no>
2019-01-12 17:45:29 +0100

    updated config with production variables ++

--- config.js
+++ config.js
@@ -2,5 +2,11 @@ module.exports = {
   development: {
     port: process.env.PORT || 3000,
     saltingRounds: 10
-  }
+  },
+  production: {
+    port: process.env.PORT || 8080,
+    saltingRounds: 10
+  },
+  tokenIssuer: 'https://api.domain.com',
+  dbString: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`
 }
