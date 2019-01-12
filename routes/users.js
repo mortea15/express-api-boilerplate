b73c7f9bf608239aa1ab96a8cae67b61329b3ff5
mortea15 <mortea15@student.uia.no>
2019-01-12 17:46:35 +0100

    cleanup and linting

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-const controller = require('../controllers/users');
-const validateToken = require('../utils').validateToken;
+const controller = require('../controllers/users')
+const validateToken = require('../utils').validateToken
 
 module.exports = (router) => {
   router.route('/users')
     .post(controller.add)
-    .get(validateToken, controller.getAll); // This route will be protected
-  
-  router.route('/login')
-    .post(controller.login);
-};
+    .get(validateToken, controller.getAll)
+
+  router.route('/auth')
+    .post(controller.login)
+}
