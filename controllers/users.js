b30fb5b1aca3d2a85fccbd57976e172847738a00
mortea15 <mortea15@student.uia.no>
2019-01-13 13:49:13 +0100

    removed returning of hashed pws, better error messages, proper sanitization of vars

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,9 @@ module.exports = {
       let result = {}
       let status = 201
       if (!err) {
-        const { email, password } = req.sanitize(req.body)
+        const email = req.sanitize(req.body.email)
+        const password = req.sanitize(req.body.password)
+
         const user = new User({ email, password })
         user.save((err, user) => {
           if (!err) {
@@ -38,43 +40,55 @@ module.exports = {
   },
 
   login: (req, res) => {
-    const { email, password } = req.sanitize(req.body)
+    const email = req.sanitize(req.body.email)
+    const password = req.sanitize(req.body.password)
 
     mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
       let result = {}
       let status = 200
       if (!err) {
-        User.findOne({ email }, (err, user) => {
+        User.findOne({ email: email }, (err, user) => {
           if (!err && user) {
-            bcrypt.compare(password, user.password).then(match => {
-              if (match) {
-                status = 200
-                const payload = { email: user.email }
-                const options = { expiresIn: '1d', issuer: config.tokenIssuer }
-                const secret = process.env.JWT_SECRET
-                const token = jwt.sign(payload, secret, options)
+            if (user.active) {
+              bcrypt.compare(password, user.password)
+                .then(match => {
+                  if (match) {
+                    status = 200
+                    const payload = { email: user.email, active: user.active, admin: user.admin }
+                    const options = { expiresIn: '1d', issuer: config.tokenIssuer }
+                    const secret = process.env.JWT_SECRET
+                    const token = jwt.sign(payload, secret, options)
 
-                result.token = token
-                result.status = status
-                result.result = user
-              } else {
-                status = 401
-                result.status = status
-                result.error = `Authentication error`
-                logger.log('error', `Status ${status} for users.login`)
-              }
-              res.status(status).send(result)
-            }).catch(err => {
-              status = 500
+                    result.token = token
+                    result.status = status
+                    user = user.toObject()
+                    delete user.password
+                    result.result = user
+                  } else {
+                    status = 401
+                    result.status = status
+                    result.error = `Authentication error`
+                    logger.log('error', `Status ${status} for users.login`)
+                  }
+                  res.status(status).send(result)
+                }).catch(err => {
+                  status = 500
+                  result.status = status
+                  result.error = 'An error occurred while authenticating the user'
+                  res.status(status).send(result)
+                  logger.log('error', `Status ${status} for users.login`)
+                })
+            } else {
+              status = 401
               result.status = status
-              result.error = err
+              result.error = 'The account needs to be activated. Please contact support.'
               res.status(status).send(result)
               logger.log('error', `Status ${status} for users.login`)
-            })
+            }
           } else {
             status = 404
             result.status = status
-            result.error = err
+            result.error = 'Invalid credentials'
             res.status(status).send(result)
             logger.log('error', `Status ${status} for users.login`)
           }
@@ -89,7 +103,6 @@ module.exports = {
     })
   },
 
-  // Todo: Protect
   getAll: (req, res) => {
     mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
       let result = {}
@@ -97,7 +110,7 @@ module.exports = {
       if (!err) {
         const payload = req.decoded
         if (payload) {
-          User.find({}, (err, users) => {
+          User.find({}, '-password', (err, users) => {
             if (!err) {
               result.status = status
               result.error = err
