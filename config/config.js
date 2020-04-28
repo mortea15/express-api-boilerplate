f0dc4d04a4796aff2309f49a0aa937d29171e83e
mortea15 <m.amundsen@pm.me>
2020-04-28 15:55:29 +0200

    changed dir structure, added more config vars

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  development: {
+    port: process.env.PORT || 3000,
+    saltingRounds: 10
+  },
+  production: {
+    port: process.env.PORT || 8080,
+    saltingRounds: 10
+  },
+  tokenIssuer: process.env.JWT_ISSUER,
+  dbString: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`,
+  API_VERSION: process.env.API_VERSION || 'v1'
+}
