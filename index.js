12b09af790e2f5d153db6e8f07ec1bbdba4ecc07
mortea15 <m.amundsen@pm.me>
2020-04-28 15:59:56 +0200

    added cors and rate limiter middleware

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const bodyParser = require('body-parser')
 const helmet = require('helmet')
 const expressSanitizer = require('express-sanitizer')
 const compression = require('compression')
+const cors = require('cors')
 
 const app = express()
 const router = express.Router()
 
-const environment = process.env.NODE_ENV
-const stage = require('./config')[environment]
-const logger = require('./helpers/logger')
-const routes = require('./routes/index.js')
+const environment = process.env.NODE_ENV || 'development'
+const stage = require('./config/config')[environment]
+const API_VERSION = require('./config/config').API_VERSION
+const logger = require('./config/logger')
+const routes = require('./routes/index')
+const rateLimiter = require('./middleware/rateLimiter')
+
+app.set('trust proxy', 1) // trust first proxy
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -20,13 +25,15 @@ app.use(expressSanitizer())
 app.use(compression())
 app.use(helmet())
 app.use(helmet.frameguard({ action: 'sameorigin' }))
+app.use(rateLimiter)
 
 if (environment !== 'production') {
   const morgan = require('morgan')
-  app.use(morgan('dev'))
+  app.use(morgan('combined'))
+  app.use(cors)
 }
 
-app.use('/v1', routes(router))
+app.use(`/${API_VERSION}`, routes(router))
 
 app.listen(`${stage.port}`, () => {
   logger.log('info', `API running on :${stage.port}`)
