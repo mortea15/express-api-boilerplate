ec105cb749d0306b1b5a323425cf56148fcf8069
mortea15 <m.amundsen@pm.me>
2020-04-29 15:49:55 +0200

    add logging to console

--- config/logger.js
+++ config/logger.js
@@ -1,3 +1,4 @@
+'use strict'
 const winston = require('winston')
 
 const logger = winston.createLogger({
@@ -9,10 +10,8 @@ const logger = winston.createLogger({
   ]
 })
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }))
-}
+logger.add(new winston.transports.Console({
+  format: winston.format.colorize()
+}))
 
 module.exports = logger
